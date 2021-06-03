70934c261d2f33652884d696acc8e33615b95cdc
hynis <85271661+hynis@users.noreply.github.com>
2021-06-03 01:49:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'I think about you all the time, you mean a lot to me 😙',
+    desc: 'damnn I believe I love u from the bottom of my heart love u with all I have ',
+    btnYes: 'Im your <33',
+    btnNo: 'Vângg, yeu anhh :3',
+    question:'Trên thế giới hơn 7 tỉ nụ cười nma với anh em là đẹp nhất quan trọng nhất <3',
+    btnReply: 'Reply cho anh nèe <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Bae ăn bánh tráng trộn nha qua anh mới tìm ra chỗ này bán nhiều vl lại còn siêu ngon.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
